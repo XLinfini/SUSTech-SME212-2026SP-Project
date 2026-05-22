a5f1e24e7e85a77b9656624d3cc0e934f7db2a50
XLinfini <whaleflot@protonmail.com>
2026-05-22 21:38:57 +0800

    debug over, complete!

--- design/source/Core.v
+++ design/source/Core.v
@@ -42,18 +42,18 @@ module Core #(
     wire issue_a_value = state == S_ISSUING;
     reg issue_a_value_d1 = 0;
     reg issue_a_value_d2 = 0;
-    reg do_win_mv = 0;
+    reg eval_vld = 0;
     reg vld_d1 = 0;
     reg vld_d2 = 0;
     reg [ROWS_ADDR_WIDTH:0] ext_row_d1 = 0;
     reg [COLS_ADDR_WIDTH:0] ext_col_d2 = 0;
     reg [COLS_ADDR_WIDTH:0] ext_col_d1 = 0;
     reg [ROWS_ADDR_WIDTH:0] ext_row_d2 = 0;
-    wire [ROWS_ADDR_WIDTH-1:0] real_row_d2 = ext_row_d2 - 2;
-    wire [COLS_ADDR_WIDTH-1:0] real_col_d2 = ext_col_d2 - 2;
+    reg [ROWS_ADDR_WIDTH-1:0] center_row_d3 = 0;
+    reg [COLS_ADDR_WIDTH-1:0] center_col_d3 = 0;
 
     // 行缓存定义
-    reg [7:0] line_buf0 [0:EXT_COLS-1]; // 不需要初始化，因为只要对应的win_vld是0，win的值就不影响结果
+    reg [7:0] line_buf0 [0:EXT_COLS-1]; // 不需要初始化，因为只要对应的line_vld_buf是0，win的值就不影响结果
     reg [7:0] line_buf1 [0:EXT_COLS-1];
     reg [7:0] line_buf2 [0:EXT_COLS-1];
     reg [7:0] line_buf3 [0:EXT_COLS-1];
@@ -66,145 +66,172 @@ module Core #(
     reg [7:0] win [0:4][0:4]; // 不需要初始化，因为只要对应的win_vld是0，win的值就不影响结果
     reg win_vld [0:4][0:4];
 
-    // 判断系统
-    function is_peak;
-        reg [11:0] outer_sum;
-        reg [4:0] outer_vld_cnt;
-        integer i, j;
-        begin
-            is_peak = 1;
-            outer_sum = 0;
-            outer_vld_cnt = 0;
+    // 判断是否是峰值
+    reg is_peak = 0;
+    reg [11:0] outer_sum;
+    reg [4:0] outer_vld_cnt;
+    reg [31:0] lhs;
+    reg [31:0] rhs;
+    integer i, j;
+    always @(*) begin
+        is_peak = 1;
+        outer_sum = 0;
+        outer_vld_cnt = 0;
+        lhs = 0;
+        rhs = 0;
 
-            for (i = 0; i <= 4; i = i + 1) begin
-                for (j = 0; j <= 4; j = j + 1) begin
-                    if ((i != 2) || (j != 2)) begin
-                        if (win_vld[i][j] && (win[i][j] >= win[2][2])) begin
-                            is_peak = 0;
-                        end
+        for (i = 0; i <= 4; i = i + 1) begin
+            for (j = 0; j <= 4; j = j + 1) begin
+                if ((i != 2) || (j != 2)) begin
+                    if (win_vld[i][j] && (win[i][j] >= win[2][2])) begin
+                        is_peak = 0;
                     end
                 end
-            end
-
-            for (i = 0; i <= 4; i = i + 1) begin
-                for (j = 0; j <= 4; j = j + 1) begin
-                    if ((i == 0 || i == 4 || j == 0 || j == 4) && win_vld[i][j]) begin
-                        outer_sum = outer_sum + win[i][j];
-                        outer_vld_cnt = outer_vld_cnt + 1;
-                    end
-                end
-            end
-
-            if (outer_vld_cnt > 0 && outer_vld_cnt * win[2][2] <= 2 * outer_sum) begin
-                is_peak = 0;
             end
         end
-    endfunction
 
-    // 插入系统
+        for (i = 0; i <= 4; i = i + 1) begin
+            for (j = 0; j <= 4; j = j + 1) begin
+                if ((i == 0 || i == 4 || j == 0 || j == 4) && win_vld[i][j]) begin
+                    outer_sum = outer_sum + win[i][j];
+                    outer_vld_cnt = outer_vld_cnt + 1;
+                end
+            end
+        end
+
+        // lhs和rhs是必须的，因为直接比较会溢出
+        lhs = outer_vld_cnt * win[2][2];
+        rhs = 2 * outer_sum;
+        if (outer_vld_cnt > 0 && lhs <= rhs) begin
+            is_peak = 0;
+        end
+    end
+    wire is_peak_real = is_peak && win_vld[2][2] && eval_vld; // 只有当中心值有效、满足峰值条件、曾经没被算过是不是峰值时，才认为是一个真正的峰值
+
+    // 判断插入位置
     reg [2:0] peak_num = 0;
     assign detect_peak_num = peak_num;
     reg [7:0] peak_vals [0:5];
     reg [ROWS_ADDR_WIDTH-1:0] peak_rows [0:5];
     reg [COLS_ADDR_WIDTH-1:0] peak_cols [0:5];
-    reg [2:0] insert_pos = 3'd7; // 0-5有效，7表示不插入
-    integer k;
-    always @(win[4][4]) begin
-        if (win_vld[2][2] && is_peak()) begin
+    reg do_insert = 0;
+    reg [2:0] insert_pos = 3'd7; // 不插入时默认为7
+    always @(*) begin
+        do_insert = 0;
+        insert_pos = 3'd7; // 默认不插入
+
+        if (is_peak_real) begin
             if (win[2][2] > peak_vals[0]) begin
+                do_insert = 1;
                 insert_pos = 0;
             end
             else if (win[2][2] > peak_vals[1]) begin
+                do_insert = 1;
                 insert_pos = 1;
             end
             else if (win[2][2] > peak_vals[2]) begin
+                do_insert = 1;
                 insert_pos = 2;
             end
             else if (win[2][2] > peak_vals[3]) begin
+                do_insert = 1;
                 insert_pos = 3;
             end
             else if (win[2][2] > peak_vals[4]) begin
+                do_insert = 1;
                 insert_pos = 4;
             end
             else if (win[2][2] > peak_vals[5]) begin
+                do_insert = 1;
                 insert_pos = 5;
             end
             else begin
+                do_insert = 0;
                 insert_pos = 7;
             end
         end
-
-        if (insert_pos >= 0 && insert_pos <= 5) begin
-            if (peak_num <= 6) begin
-                peak_num = peak_num + 1;
-            end
-            
-            for (k = 5; k > insert_pos; k = k - 1) begin
-                peak_vals[k] = peak_vals[k-1];
-                peak_rows[k] = peak_rows[k-1];
-                peak_cols[k] = peak_cols[k-1];
-            end
-            peak_vals[insert_pos] = win[2][2];
-            peak_rows[insert_pos] = real_row_d2 - 2;
-            peak_cols[insert_pos] = real_col_d2 - 2;
-        end
     end
 
-    // 时序逻辑：进、移、迭
+    // 时序逻辑：延、进、移、插、迭
+    /*
+        状态机只管“发不发地址”和“什么时候结束”
+        延迟管道只管“地址/valid 和 BRAM data 对齐”
+        pix_cycle 只管“窗口何时移动”
+        eval_valid 只管“何时检测窗口”
+        do_insert 只管“何时更新 Top-6”
+    */
     integer m, n;
+    integer k;
     always @(posedge clk) begin
+        // 延迟管道更新
         if (state == S_ISSUING || state == S_DRAIN) begin
-            // 新的数据进入窗口
-            if (vld_d2 == 0) begin
-                win[4][4] <= 0;
-            end
-            else begin
-                win[4][4] <= bram_rd_data;
-            end
-            win_vld[4][4] <= vld_d2;
-
-            // 窗口和行缓存移动
-            if (do_win_mv) begin
-                line_buf0[ext_col_d2] <= win[4][4];
-                line_buf1[ext_col_d2] <= line_buf0[ext_col_d2];
-                line_buf2[ext_col_d2] <= line_buf1[ext_col_d2];
-                line_buf3[ext_col_d2] <= line_buf2[ext_col_d2];
-
-                line_vld_buf0[ext_col_d2] <= win_vld[4][4];
-                line_vld_buf1[ext_col_d2] <= line_vld_buf0[ext_col_d2];
-                line_vld_buf2[ext_col_d2] <= line_vld_buf1[ext_col_d2];
-                line_vld_buf3[ext_col_d2] <= line_vld_buf2[ext_col_d2];
-
-                for (m = 0; m <= 4; m = m + 1) begin
-                    for (n = 0; n <= 3; n = n + 1) begin
-                        win[m][n] <= win[m][n+1];
-                        win_vld[m][n] <= win_vld[m][n+1];
-                    end
-                end
-
-                win[0][4] <= line_buf3[ext_col_d2];
-                win[1][4] <= line_buf2[ext_col_d2];
-                win[2][4] <= line_buf1[ext_col_d2];
-                win[3][4] <= line_buf0[ext_col_d2];
-
-                win_vld[0][4] <= line_vld_buf3[ext_col_d2];
-                win_vld[1][4] <= line_vld_buf2[ext_col_d2];
-                win_vld[2][4] <= line_vld_buf1[ext_col_d2];
-                win_vld[3][4] <= line_vld_buf0[ext_col_d2];
-            end
-
-            // 延迟系统
             issue_a_value_d1 <= issue_a_value;
             issue_a_value_d2 <= issue_a_value_d1;
-            do_win_mv <= issue_a_value_d2;
+            eval_vld <= issue_a_value_d2;
             vld_d1 <= vld;
             vld_d2 <= vld_d1;
             ext_row_d1 <= ext_row;
             ext_row_d2 <= ext_row_d1;
+            center_row_d3 <= ext_row_d2 - 4;
             ext_col_d1 <= ext_col;
             ext_col_d2 <= ext_col_d1;
+            center_col_d3 <= ext_col_d2 - 4;
         end
 
+        // 数据流动
+        if (issue_a_value_d2) begin
+            // 新的数据进入窗口
+            win[4][4] <= vld_d2 ? bram_rd_data : 0;
+            win_vld[4][4] <= vld_d2;
+
+            // 窗口和行缓存移动
+            line_buf0[ext_col_d2] <= vld_d2 ? bram_rd_data : 0;
+            line_buf1[ext_col_d2] <= line_buf0[ext_col_d2];
+            line_buf2[ext_col_d2] <= line_buf1[ext_col_d2];
+            line_buf3[ext_col_d2] <= line_buf2[ext_col_d2];
+
+            line_vld_buf0[ext_col_d2] <= vld_d2;
+            line_vld_buf1[ext_col_d2] <= line_vld_buf0[ext_col_d2];
+            line_vld_buf2[ext_col_d2] <= line_vld_buf1[ext_col_d2];
+            line_vld_buf3[ext_col_d2] <= line_vld_buf2[ext_col_d2];
+
+            for (m = 0; m <= 4; m = m + 1) begin
+                for (n = 0; n <= 3; n = n + 1) begin
+                    win[m][n] <= win[m][n+1];
+                    win_vld[m][n] <= win_vld[m][n+1];
+                end
+            end
+
+            win[0][4] <= line_buf3[ext_col_d2];
+            win[1][4] <= line_buf2[ext_col_d2];
+            win[2][4] <= line_buf1[ext_col_d2];
+            win[3][4] <= line_buf0[ext_col_d2];
+
+            win_vld[0][4] <= line_vld_buf3[ext_col_d2];
+            win_vld[1][4] <= line_vld_buf2[ext_col_d2];
+            win_vld[2][4] <= line_vld_buf1[ext_col_d2];
+            win_vld[3][4] <= line_vld_buf0[ext_col_d2];
+        end
+
+        // 峰值插入
+        if (do_insert) begin
+            if (peak_num < 6) begin // 这个判断是必须的，万一peak_num已经满了但新来的值又比第6大的话，peak_num还在加，就爆了
+                peak_num <= peak_num + 1;
+            end
+
+            for (k = 5; k > 0; k = k - 1) begin
+                if (k > insert_pos) begin
+                    peak_vals[k] <= peak_vals[k-1];
+                    peak_rows[k] <= peak_rows[k-1];
+                    peak_cols[k] <= peak_cols[k-1];
+                end
+            end
+            peak_vals[insert_pos] <= win[2][2];
+            peak_rows[insert_pos] <= center_row_d3;
+            peak_cols[insert_pos] <= center_col_d3;
+        end
+
+        // 状态机
         case (state)
             S_ISSUING: begin
                 // 坐标迭代（只在S_ISSUING状态下进行）
@@ -225,7 +252,7 @@ module Core #(
             S_DRAIN: begin
                 // 如果是在S_DRAIN状态，不迭代坐标，等待缓冲区数据排空即可
                 if (state == S_DRAIN) begin
-                    if (issue_a_value_d1 == 0 && issue_a_value_d2 == 0) begin
+                    if (issue_a_value_d1 == 0 && issue_a_value_d2 == 0 && eval_vld == 0) begin
                         state <= S_DONE;
                     end
                 end
@@ -244,6 +271,7 @@ module Core #(
                 ext_col <= 0;
                 issue_a_value_d1 <= 0;
                 issue_a_value_d2 <= 0;
+                eval_vld <= 0;
                 vld_d1 <= 0;
                 vld_d2 <= 0;
                 ext_row_d1 <= 0;
@@ -286,6 +314,7 @@ module Core #(
             ext_col <= 0;
             issue_a_value_d1 <= 0;
             issue_a_value_d2 <= 0;
+            eval_vld <= 0;
             vld_d1 <= 0;
             vld_d2 <= 0;
             ext_row_d1 <= 0;
@@ -315,7 +344,7 @@ module Core #(
     end
 
     assign detect_finish = (state == S_DONE);
-    
+
     // 输出系统
     reg [ROWS_ADDR_WIDTH-1:0] disp_peak_row_reg;
     reg [COLS_ADDR_WIDTH-1:0] disp_peak_col_reg;
@@ -324,7 +353,7 @@ module Core #(
     assign disp_peak_row = disp_peak_row_reg;
     assign disp_peak_val = disp_peak_val_reg;
     always @(*) begin
-        if (disp_peak_idx <= peak_num - 1) begin
+        if (disp_peak_idx < peak_num) begin
             disp_peak_row_reg = peak_rows[disp_peak_idx];
             disp_peak_col_reg = peak_cols[disp_peak_idx];
             disp_peak_val_reg = peak_vals[disp_peak_idx];
